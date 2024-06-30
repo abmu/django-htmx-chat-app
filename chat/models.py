9b90fa34c0259802a22e28686f763d4bdfab03c0
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2024-06-30 20:47:54 +0100

    Fixed message model

--- chat/models.py
+++ chat/models.py
@@ -4,7 +4,17 @@ from django.utils import timezone
 
 
 class Message(models.Model):
-    sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL)
-    recipient = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL)
+    sender = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='sent_messages'
+    )
+    recipient = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='received_messages'
+    )
     content = models.TextField()
     timestamp = models.DateTimeField(default=timezone.now)
