04a7fd7ee3b2555b9b4e5c2887b6b71b86c8dc0d
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2024-07-01 18:30:50 +0100

    Message form

--- chat/views.py
+++ chat/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .forms import MessageForm
 
 
 @login_required(redirect_field_name=None)
 def home(request):
-    return render(request, 'chat/home.html')
+    if request.method == 'POST':
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            form.save()
+            return redirect('chat_home')
+    else:
+        form = MessageForm()
+    
+    return render(request, 'chat/home.html', {
+        'title': 'Home',
+        'form': form
+    })
