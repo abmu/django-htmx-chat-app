795ce988f4de27f265f9ed3df2bef7fabd25df7f
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2024-07-15 17:49:38 +0100

    Installed channels and daphne

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -158,3 +159,6 @@ LOGIN_URL = 'account_login'
 EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 
 EMAIL_FILE_PATH = BASE_DIR / 'emails'
+
+
+ASGI_APPLICATION = 'config.asgi.application'
